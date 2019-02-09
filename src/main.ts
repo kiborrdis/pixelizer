38076b92581f31877bb5f0f54481108712c75c0e
radiansz <lightwaveinthespace@gmail.com>
2019-02-09 07:00:16 +0300

    Export Action interface

--- src/main.ts
+++ src/main.ts
@@ -3,3 +3,4 @@ export * from './BrowserInteractionAdapter';
 export * from './recorders/index';
 export * from './tools/index';
 export * from './ActionSerializer';
+export { Action } from './interfaces/Action';
